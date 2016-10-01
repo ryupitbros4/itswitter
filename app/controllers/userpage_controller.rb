d72358e4309e7e8399529aa78fc1b0de792f648c
itomasameteo <k168586@ie.u-ryukyu.ac.jp>
2016-10-01 06:32:04 +0000

    sort

--- app/controllers/userpage_controller.rb
+++ app/controllers/userpage_controller.rb
@@ -1,7 +1,12 @@
+# -*- coding:utf-8 -*-
 class UserpageController < ApplicationController
   def display
     @ranking = Jam.all
     
     
+    @subrank = [[2,"あがり"],[1,"さんちくじゅ"],[3,"がりゅうや"],[5,"とりたま"],[4,"通堂"]]
+    @sortrank = @subrank.sort
+    
+    #redirect_to '/userpage/display'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   get 'userpage/display'
   root "userpage#display"
+  
   #root 'home#index'  
   
   # The priority is based upon order of creation: first created -> highest priority.
