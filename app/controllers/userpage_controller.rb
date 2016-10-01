e016cd123c95b44e600ee948fada103a69ec22d9
itomasameteo <k168586@ie.u-ryukyu.ac.jp>
2016-10-01 03:54:57 +0000

    display syuusei

--- app/controllers/userpage_controller.rb
+++ app/controllers/userpage_controller.rb
@@ -1,4 +1,8 @@
 class UserpageController < ApplicationController
   def display
+    @ranking = Jam.all
+    @ranking.title = params[:tweet][:title]
+    @ranking.content = params[:tweet][:content]
+    @ranking.save
   end
 end
