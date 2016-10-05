1693c440912d9cc2273af360c85cf7f6bcc240ba
Keito <e125762@ie.u-ryukyu.ac.jp>
2016-10-05 06:10:18 +0000

    display sorting seats_occ

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class RestaurantsController < ApplicationController
 
   def index
@@ -5,12 +6,15 @@ class RestaurantsController < ApplicationController
     @per_array = Array.new
     
     @restaurants.each do |restaurant|
+#perは占有率
       per = ((restaurant.num_people.to_f/restaurant.num_seats.to_f)*100).round
-      @per_array.push([per,restaurant.name])
+#モデルのseats_occに占有率を更新
+      restaurant.seats_occ = per
+      restaurant.save
     end
-    
-    @rank = @per_array.sort
-    @len_num = @rank.length - 1
+#占有率が低い順に並び替える
+    @rank=Restaurant.order('seats_occ')
+    @len_num = @rank.count
   end
 
 end
