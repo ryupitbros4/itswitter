c74817aa6340c134273b8fd3c38a4c6f74ca02a5
Keito <e125762@ie.u-ryukyu.ac.jp>
2016-10-05 00:08:45 +0000

    show percentage and add restaurant model,controller again commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001020010) do
+ActiveRecord::Schema.define(version: 20161004100957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20161001020010) do
   create_table "jams", force: :cascade do |t|
     t.text     "store"
     t.integer  "congestion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "num_seats"
+    t.integer  "num_people"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
