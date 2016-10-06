83f63d876a6fa0595b0abf6c0ae4022951053677
itomasameteo <k168586@ie.u-ryukyu.ac.jp>
2016-10-06 15:42:54 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005124151) do
+ActiveRecord::Schema.define(version: 20161006151641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jams", force: :cascade do |t|
+    t.text     "store"
+    t.integer  "congestion"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
