0711af5d37d8b26664880fb9a9c731cd71f6fe7b
itomasameteo <k168586@ie.u-ryukyu.ac.jp>
2016-10-07 02:23:39 +0000

    display time

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20161004100957) do
-=======
 ActiveRecord::Schema.define(version: 20161006095320) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
