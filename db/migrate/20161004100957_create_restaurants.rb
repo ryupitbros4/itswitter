646405b9106be9b054f52dfc9236b170cd4b964d
itomasameteo <k168586@ie.u-ryukyu.ac.jp>
2016-10-08 06:42:55 +0000

    default 0

--- db/migrate/20161004100957_create_restaurants.rb
+++ db/migrate/20161004100957_create_restaurants.rb
@@ -3,8 +3,8 @@ class CreateRestaurants < ActiveRecord::Migration
     create_table :restaurants do |t|
       t.string :name
       t.integer :num_seats
-      t.integer :num_people
-      t.integer :seats_occ
+      t.integer :num_people, default: 0
+      t.integer :seats_occ, default: 0
 
       t.timestamps null: false
     end
