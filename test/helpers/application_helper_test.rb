dc20a01a8a8935a8add6eb4a4139b3f86a94090c
hogehiga <hogehiga@gmail.com>
2016-10-13 02:57:37 +0900

    Fit to specification(probably).
    I'm not very familiar with spec...
    But, I beleve it is correct that floor seconds if seconds bigger than 60.

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -10,12 +10,12 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_equal "1分", sec2h(60)
   end
   test "1分より長くて1時間未満の変換" do
-    assert_equal "1分8秒", sec2h(68)
+    assert_equal "1分", sec2h(68)
   end
   test "1時間の変換" do
-    assert_equal "1時間", sec2h(3600)
+    assert_equal "1時間0分", sec2h(3600)
   end
   test "1時間より長い変換" do
-    assert_equal "1時間1分40秒", sec2h(3700)
+    assert_equal "1時間1分", sec2h(3700)
   end
 end
