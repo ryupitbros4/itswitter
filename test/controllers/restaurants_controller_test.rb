41c9257bc130b7001ba7304095e460fd973f0acd
hogehiga <hogehiga@gmail.com>
2016-10-12 18:17:19 +0900

    Modify test.
    Resistant to changing view.

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -8,12 +8,10 @@ class RestaurantsControllerTest < ActionController::TestCase
   test "初期登録された店が表示されている" do
     get :index
     assert_response :success
-    assert_select 'center h1', 7
-    assert_select 'center h1' do |item|
-      item.each_with_index do |h1, index|
-        next if index < 2
-        assert_match /^.*(あがり|通堂|我流屋|三竹寿|鳥玉).*$/, h1
-      end
+    shop_names_in_view = assigns(:rank).map(&:name)
+    shops = %w(あがり 通堂 我流屋 三竹寿 鳥玉)
+    shops.each do |shop|
+      shop_names_in_view.include?(/^.*#{shop}.*$/)
     end
   end
 end
