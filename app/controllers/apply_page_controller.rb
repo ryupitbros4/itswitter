3c166320a84606b8cd14b417dc4f48dbccff034c
Ito Takumi <e125743@ie.u-ryukyu.ac.jp>
2016-10-14 07:59:02 +0000

    ValidationError

--- app/controllers/apply_page_controller.rb
+++ app/controllers/apply_page_controller.rb
@@ -5,8 +5,14 @@ class ApplyPageController < ApplicationController
   end
 
   def new
-    @apply = Apply.create(params.require(:apply).permit(:apply_restaurant, :free))
-    redirect_to :apply_page_index
+    @apply = Apply.new(params.require(:apply).permit(:apply_restaurant, :free))
+    if @apply.save
+      redirect_to :apply_page_index
+    else
+      @applies = Apply.all
+      render 'index'
+    end
+
   end
 
 end
