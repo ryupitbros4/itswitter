87401b850005da187d7414398371b8a4b01696f9
itomasameteo <k168586@ie.u-ryukyu.ac.jp>
2016-10-20 03:30:06 +0000

    add code2

--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -1,15 +1,15 @@
 class DemandsController < ApplicationController
   def index
-    #@demand = Demand.new
+    @demand = Demand.new
     @demands = Demand.all.order(id: :desc)
   end
 
   def new
-    @demand = Demand.new(params.require(:apply).permit(:demand_restaurant, :free))
+    @demand = Demand.new(params.require(:demand).permit(:demand_restaurant, :free))
     if @demand.save
       redirect_to :demands_index
     else
-      @applies = Demand.all.order(id: :desc)
+      @demands = Demand.all.order(id: :desc)
       render 'index'
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020014316) do
+ActiveRecord::Schema.define(version: 20161020031659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "demands", force: :cascade do |t|
     t.text     "free"
-    t.string   "apply_restaurant"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.string   "demand_restaurant"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
-  
+
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.integer  "num_seats"
