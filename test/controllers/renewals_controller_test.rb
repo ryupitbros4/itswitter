7af1dfa022ea94f0065292d2be563aa75a9c2006
hogehiga <hogehiga@gmail.com>
2016-10-29 22:42:15 +0900

    Add tests.

--- test/controllers/renewals_controller_test.rb
+++ test/controllers/renewals_controller_test.rb
@@ -4,4 +4,15 @@ class RenewalsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "一覧画面で更新情報が取得できる" do
+    get :index
+    assert_equal 2, assigns(:renewals).length
+  end
+
+  test "詳細画面で更新情報が取得できる" do
+    one = renewals(:one)
+    get :show, id: one.id
+    assert_equal "更新情報_1", assigns(:renewal).update_info
+  end
 end
