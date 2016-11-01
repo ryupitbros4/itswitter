2e8f04c496676d6ede9bfe379f709daf6900e00b
Ito Takumi <e125743@ie.u-ryukyu.ac.jp>
2016-11-01 08:55:06 +0000

    limitedRestaurantsRanking

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -28,7 +28,7 @@ class RestaurantsController < ApplicationController
       end
     end
     #占有率が低い順に並び替える
-    @rank=Restaurant.order('crowdedness')
+    @rank=Restaurant.order('crowdedness').limit(10)
 
     @how_crowded = ["席がガラガラ","席が半分埋まってる","席がほぼ埋まってる","席に座れない人がいる","席に座れない人がかなりいる","CLOSE","記録なし"]
     @crowded_image = ["garagara","yayakomi","komi","yayamachi","machi","close2","close"]
