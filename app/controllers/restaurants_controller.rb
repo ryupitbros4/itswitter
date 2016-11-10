25cebf91de4580f67cd55ca208f6174d39c4313d
Ito Takumi <e125743@ie.u-ryukyu.ac.jp>
2016-11-10 14:49:45 +0000

    DeleteMisstake

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,7 +11,7 @@ class RestaurantsController < ApplicationController
   
   def index
     @restaurants = Restaurant.all
-    @new_restaurants = Restaurant.where('created_at > ?', params[:from] ? params[:from] : 30.days.ago).order(created_at: :desc).order("created_at desc")
+    @new_restaurants = Restaurant.where('created_at > ?', params[:from] ? params[:from] : 30.days.ago).order(created_at: :desc)
     @per_array = Array.new
     
     #占有率が低い順に並び替える
