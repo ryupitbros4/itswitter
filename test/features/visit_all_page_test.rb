2cfe9ea2650fb233992cec2d78286f127dec3b97
hogehiga <hogehiga@gmail.com>
2016-11-12 08:35:33 +0900

    Modify test.
    Visit all page(logged in or logged out, start visit at top or admin page).

--- test/features/visit_all_page_test.rb
+++ test/features/visit_all_page_test.rb
@@ -1,20 +1,38 @@
 require "test_helper"
 
 feature "VisitAllPage" do
-  scenario "全てのリンクからページを辿ってエラーが起きないことを確認する" do
+  scenario "ログインしていない状態でトップからリンクを辿ってエラーが起きないことを確認する" do
     visit root_path
-    visited = []
-    visit_all_links(visited)
+    visit_all_links([])
+  end
+
+  scenario "ログインした状態でトップからリンクを辿ってエラーが起きないことを確認する" do
+    login(cap: true)
+    visit root_path
+    visit_all_links([])
+  end
+
+  scenario "ログインしていない状態で管理画面からリンクを辿ってエラーが起きないことを確認する" do
+    visit admin_index_path
+    visit_all_links([])
+  end
+
+  scenario "ログインした状態で管理画面からリンクを辿ってエラーが起きないことを確認する" do
+    login(cap: true)
+    visit admin_index_path
+    visit_all_links([])
   end
 
   private
   def visit_all_links(visited)
     return if visited.include? current_path
-    visited << current_path
-    all('a').each do |a|
-      if a[:href].start_with?('/')
-        visit a[:href]
-        visit_all_links(visited)
+    assert_nothing_raised do
+      visited << current_path
+      all('a').each do |a|
+        if a[:href].start_with?('/')
+          visit a[:href]
+          visit_all_links(visited)
+        end
       end
     end
   end
