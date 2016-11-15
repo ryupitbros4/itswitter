7576b7a5b5ba4c21f2041297b08fbdacfa8455f4
itomasameteo <k168586@ie.u-ryukyu.ac.jp>
2016-11-15 11:03:16 +0000

    change yml

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,17 +3,10 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user1 = User.new({provider: 'twitter', uid: '1234567890', nickname: '1たろう', image_url: '', point: '111'})
-    @user1.save
-    @user2 = User.new({provider: 'twitter', uid: '2345678901', nickname: '2たろう', image_url: '', point: '222'})
-    @user2.save
-    @user3 = User.new({provider: 'twitter', uid: '3456789012', nickname: '3たろう', image_url: '', point: '333'})
-    @user3.save
-    
-    @user_tai1 =  User.new({provider: 'twitter', uid: '12345678901', nickname: 'タイたろう', image_url: '', point: '1'})
-    @user_tai1.save
-    @user_tai2 =  User.new({provider: 'twitter', uid: '12345678902', nickname: 'タイたろう2', image_url: '', point: '1'})
-    @user_tai2.save
+    @user1 = User.find_by(uid: 12345)
+    @user2 = User.find_by(uid: 67890)
+    @user3 = User.find_by(uid: 45678)
+    @user4 = User.find_by(uid: 89012)
   end
 
   test "ユーザーランキングの順位が正しいか" do
@@ -23,7 +16,7 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "同率ポイントユーザーの順位はタイか" do
-    assert_equal @user_tai1.rank, @user_tai2.rank
+    assert_equal @user1.rank, @user4.rank
   end
   
 end
