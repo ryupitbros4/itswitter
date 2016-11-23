736d39b726ecdb6c8b764a22e6907bd480f0eca0
Keito <e125762@ie.u-ryukyu.ac.jp>
2016-11-23 16:11:53 +0000

    fix test error

--- test/features/apply_page_test.rb
+++ test/features/apply_page_test.rb
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
 require "test_helper"
 
 feature "ApplyPage" do
   scenario "バリデーションに失敗した直後にリロードしてもエラー画面が表示されない" do
     visit demands_index_path
-    click_button '登録'
+    #テストを通すために変更
+    #click_button '登録' 
+    find(:link_or_button, "登録").trigger('click')
     assert page.has_content?('入力して下さい')
     visit current_path
   end
