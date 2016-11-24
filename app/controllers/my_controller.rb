d30366d979bf1be103179d1c5bb3f701772be402
hogehiga <hogehiga@gmail.com>
2016-11-25 02:19:05 +0900

    Do not reload page when follow and unfollow.

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -11,13 +11,19 @@ class MyController < ApplicationController
     unless current_user.restaurants.include?(Restaurant.find(params[:restaurant_id]))
       current_user.restaurants << Restaurant.find(params[:restaurant_id])
     end
-    redirect_to :back
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js { render 'my/follow', locals: { restaurant: Restaurant.find(params[:restaurant_id]) } }
+    end
   end
 
   def unfollow
     if current_user.restaurants.include?(Restaurant.find(params[:restaurant_id]))
       current_user.restaurants.destroy(params[:restaurant_id])
     end
-    redirect_to :back
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js { render 'my/unfollow', locals: { restaurant: Restaurant.find(params[:restaurant_id]) } }
+    end
   end
 end
