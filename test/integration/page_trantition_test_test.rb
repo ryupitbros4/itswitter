728a722d0c792d5c4de5f9c46796b20d979665d5
itomasameteo <k168586@ie.u-ryukyu.ac.jp>
2016-12-05 13:56:24 +0000

    fix F

--- test/integration/page_trantition_test_test.rb
+++ test/integration/page_trantition_test_test.rb
@@ -5,8 +5,12 @@ class PageTrantitionTestTest < ActionDispatch::IntegrationTest
   test "ログインしてページを遷移したときに、ログイン情報が保持されている" do
     login
     get '/'
-    assert_select 'ul[class="nav navbar-nav"] li:nth-child(3)', 'ログアウト'
+    #assert_select 'ul[class="nav navbar-nav"] li:nth-child(3)', 'ログアウト'
+    #get '/demands/index'
+    #assert_select 'ul[class="nav navbar-nav"] li:nth-child(3)', 'ログアウト'
+    
+    assert_select 'div[class="navbar-left"] div:nth-child(3)', 'ログアウト'
     get '/demands/index'
-    assert_select 'ul[class="nav navbar-nav"] li:nth-child(3)', 'ログアウト'
+    assert_select 'div[class="navbar-left"] div:nth-child(3)', 'ログアウト'
   end
 end
