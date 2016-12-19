2f2c0e09e59b5a7de6c5867fa66a9a0548ff99cf
Ito Takumi <e125743@ie.u-ryukyu.ac.jp>
2016-12-19 11:13:37 +0000

    InsertFollowListInMyPage

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class MyController < ApplicationController
 
   before_action :authenticate_user!
@@ -5,6 +6,9 @@ class MyController < ApplicationController
   def index
     set_crowded_consts
     @my = User.find(session[:user_id])
+    @postit = @my.comments
+    @crowded_image = ["garagara","yayakomi","komi","yayamachi","machi","close2","close"]
+    @how_crowded = ["席がガラガラ","席が半分埋まってる","席がほぼ埋まってる","席に座れない人がいる","席に座れない人がかなりいる","CLOSE","記録なし"]
   end
 
   def favorites
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :restaurants, :through => :comments
+
   has_many :favorite_restaurants
   has_many :restaurants, :through => :favorite_restaurants
 
   has_many :pressed_users
+  has_many :comments, :through => :pressed_users
 
   def self.find_or_create_from_auth(auth)
     provider = auth[:provider]
