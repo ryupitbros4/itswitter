7bfb0689ccbd1b6b377096e71a223e1691716df0
itomasameteo <k168586@ie.u-ryukyu.ac.jp>
2017-02-13 11:14:26 +0000

    change jquery button

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -249,25 +249,38 @@ class RestaurantsController < ApplicationController
   def all_rest
     @restaurants = Restaurant.all.restaurant_order_hurigana
     @per_array = Array.new
-    @next_index = params[:pre_index]
-    if @next_index.blank?
-      @next_index = 0
-    end
-    
-    @initial_word = params[:pre_index2]
-    if @initial_word.blank?
-      @initial_word = 0
-    end
-      
+    @next_index = 0
+
     #五十音表の配列を宣言
     @gozyuuonn = ["あ","か","さ","た","な","は","ま","や","ら","わ"]
     @gozyuuonn2 = [["あ", "い", "う", "え", "お"], ["か", "き", "く", "け", "こ"], ["さ", "し", "す", "せ", "そ"], ["た", "ち", "つ", "て", "と"], ["な", "に", "ぬ", "ね", "の"], ["は", "ひ","ふ", "へ", "ほ"], ["ま", "み", "む", "め", "も"], ["や", "ゆ", "よ"], ["ら", "り", "る", "れ", "ろ"], ["わ", "を", "ん"]]
-    @gozyuuonnd = [["が", "ぎ", "ぐ", "げ", "ご"],["ざ", "じ", "ず", "ぜ", "ぞ"],["だ", "ぢ", "づ", "で", "ど"],["ぱ", "ぴ","ぷ", "ぺ", "ぽ"],["ば", "び","ぶ", "べ", "ぼ"]]
-    
     @how_crowded = ["席がガラガラ","席が半分埋まってる","席がほぼ埋まってる","席に座れない人がいる","席に座れない人がかなりいる","CLOSE","記録なし"]
     @crowded_image = ["garagara","yayakomi","komi","yayamachi","machi","close2","close"]
   end
   
+  def rest_ind
+    @gozyuuonn2 = [["あ", "い", "う", "え", "お"], ["か", "き", "く", "け", "こ"], ["さ", "し", "す", "せ", "そ"], ["た", "ち", "つ", "て", "と"], ["な", "に", "ぬ", "ね", "の"], ["は", "ひ","ふ", "へ", "ほ"], ["ま", "み", "む", "め", "も"], ["や", "ゆ", "よ"], ["ら", "り", "る", "れ", "ろ"], ["わ", "を", "ん"]]
+    
+    if params["pre_id"].blank?
+      @next_index = 0
+    else
+      @next_index = params["pre_id"]
+    end
+    render :rest_ind
+  end
+  
+  def rest_show
+    @crowded_image = ["garagara","yayakomi","komi","yayamachi","machi","close2","close"]
+    
+    if params["res_name"].blank?
+      @name = ""
+    else
+      @name = params["res_name"]
+    end
+    
+    @restaurants_initial = Restaurant.initial_search(@name)
+    render :rest_show
+  end 
   
   private
   
